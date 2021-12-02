b7eef99f199092a2592224f9b520a927f47682e2
vapormusic <vietanhfat@gmail.com>
2021-12-02 12:07:07 +0700

    show this

--- lib/webview.dart
+++ lib/webview.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class WebViewScreen extends StatefulWidget {
@@ -20,11 +21,21 @@ class WebViewExampleState extends State<WebViewScreen> {
 
   @override
   Widget build(BuildContext context) {
+    Fluttertoast.showToast(
+        msg: widget.ip.replaceAll("%3", ''),
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0);
     return WillPopScope(
         onWillPop: () async => false,
-        child: WebView(
-          javascriptMode: JavascriptMode.unrestricted,
-          initialUrl: widget.ip.replaceAll("%3", ''),
-        ));
+        child: Column(children: [
+          WebView(
+            javascriptMode: JavascriptMode.unrestricted,
+            initialUrl: widget.ip.replaceAll("%3", ''),
+          ),
+        ]));
   }
 }
